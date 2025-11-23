62c0fca6125d6497510b0e6bad5748dd27e9a180
agathe.coulombier <agathe.coulombier@foodcheri.com>
2025-11-24 00:28:36 +0100

    last try

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,10 +1,7 @@
 import {getRequestConfig} from 'next-intl/server';
 
-export default getRequestConfig(async ({requestLocale}) => {
-  const locale = (await requestLocale) || 'en';
-  
+export default getRequestConfig(async ({locale}) => {
   return {
-    locale,
     messages: (await import(`./messages/${locale}.json`)).default
-  };
+  } as any;
 });

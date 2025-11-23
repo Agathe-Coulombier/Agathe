ec84edece8f82d58086fa5c001a05b878b3af067
agathe.coulombier <agathe.coulombier@foodcheri.com>
2025-11-24 00:31:26 +0100

    jpp

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,9 @@
 import {getRequestConfig} from 'next-intl/server';
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({requestLocale}) => {
+  // This will correspond to the `[locale]` segment from the URL
+  const locale = await requestLocale;
+  
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   } as any;

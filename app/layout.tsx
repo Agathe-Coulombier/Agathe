a42ee3d9c9e17db8b1c7245e2b3c410f190b377f
agathe.coulombier <agathe.coulombier@foodcheri.com>
2025-11-24 00:15:36 +0100

    test 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Agathe',
+  description: 'Web Developer Portfolio'
+};
+
 export default function RootLayout({
   children,
 }: {

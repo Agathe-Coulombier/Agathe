3b48c08cc8adcb24207881383b7d5b2289b3197a
agathe.coulombier <agathe.coulombier@foodcheri.com>
2025-11-24 00:36:32 +0100

    dying

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,14 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 
 export default getRequestConfig(async ({requestLocale}) => {
   // This will correspond to the `[locale]` segment from the URL
-  const locale = await requestLocale;
+  let locale = await requestLocale;
+  
+  // Ensure we have a valid locale
+  if (!locale || !['en', 'fr'].includes(locale)) {
+    locale = 'en';
+  }
   
   return {
     messages: (await import(`./messages/${locale}.json`)).default
